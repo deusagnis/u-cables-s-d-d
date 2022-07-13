cc9f65dbe486ee292544e91d80d946944399869c
Deusagnis <deusagnis@gmail.com>
2022-07-14 01:25:13 +0300

    Add ability of different attack methods.

--- src/AttackTargets.js
+++ src/AttackTargets.js
@@ -1,7 +1,11 @@
+import FireTorpedoes from "./FireTorpedoes.js";
+import AttackByDetachableExplosive from "./AttackByDetachableExplosive.js";
+
 /**
  * Произвести атаку целей торпедами.
  */
 export default class AttackTargets {
+
     /**
      * Интерфейс системы навигации.
      */
@@ -15,69 +19,41 @@ export default class AttackTargets {
      * @type {[]}
      */
     targetsPositions = []
-
-    /**
-     * Время самой долгого запуска торпеды.
-     * @type {number}
-     */
-    maxLaunchTime = 0
     /**
      * Результат поражения целей.
      * @type {[]}
      */
     result = []
+
     /**
      * Итоговое время затраченное на атаку.
      */
     attackTime
 
     /**
-     * Атаковать цели.
+     * Выполнение атаки цели в зависимости от выбранного способа атаки в боевой системе.
      */
-    attack() {
-        this.launchTorpedoes()
-        this.calcAttackTime()
-    }
-
-    /**
-     * Запустить торпеды.
-     */
-    launchTorpedoes() {
-        for (let target of this.targetsPositions) {
-            const time = this.calcTorpedoAttackTime(target.sector.position)
-            if (time > this.maxLaunchTime) this.maxLaunchTime = time
-            const defeated = this.fireTorpedo()
-
-            this.addAttackResult(target, defeated, time)
+    attack(){
+        let attacker
+        switch (this.combatSystem.currentAttackWay){
+            case 'detachableExplosive':
+                attacker = new AttackByDetachableExplosive()
+                attacker.combatSystem = this.combatSystem
+                attacker.navigationSystem = this.navigationSystem
+                attacker.targetsPositions = this.targetsPositions
+                attacker.attackInspector = this
+                attacker.attack()
+                break
+            default:
+                attacker = new FireTorpedoes()
+                attacker.combatSystem = this.combatSystem
+                attacker.navigationSystem = this.navigationSystem
+                attacker.targetsPositions = this.targetsPositions
+                attacker.attackInspector = this
+                attacker.fire()
+                break
         }
-    }
-
-    /**
-     * Посчитать время торпедной атаки для заданной цели.
-     * @param targetPosition
-     * @returns {number}
-     */
-    calcTorpedoAttackTime(targetPosition) {
-        const distance = this.navigationSystem.constructor.calcDistance(
-            this.navigationSystem.position,
-            targetPosition
-        )
-        return Math.round(distance / this.combatSystem.torpedo.speed)
-    }
-
-    /**
-     * Успех запуска торпеды.
-     * @returns {boolean}
-     */
-    fireTorpedo() {
-        return Math.random() < this.combatSystem.torpedo.hitProbability
-    }
-
-    /**
-     * Вычисения общего времени атаки.
-     */
-    calcAttackTime() {
-        this.attackTime = this.combatSystem.torpedo.launchTime * this.targetsPositions.length + this.maxLaunchTime;
+        this.attackTime = attacker.attackTime
     }
 
     /**
@@ -94,5 +70,4 @@ export default class AttackTargets {
             attackPosition: this.navigationSystem.position
         })
     }
-
 }
